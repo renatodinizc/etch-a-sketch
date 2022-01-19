109ed2c128fc08a4addd701108483fb3272140ab
Renato Diniz <renatodiniz25@gmail.com>
2022-01-19 19:55:34 -0300

    Implement board reset + size option

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 'use strict';
 const board = document.getElementById('board');
-let size = 30;
+let size = 16;
 
-generateBoard(16);
+generateBoard(size);
 
 function generateBoard(size) {
   for(let i = 0; i < size; i++) {
     const div = document.createElement('div');
     const column = board.appendChild(div);
     column.classList.add('column')
-    column.classList.add(i)
     for(let j = 0; j < size; j++) {
       const div = document.createElement('div');
       const square = column.appendChild(div);
-      square.classList.add('square');
-      square.classList.add(`${j}x${i}`);
+      square.classList.add('square');;
     }
+  }
+  const squares = document.getElementsByClassName('square');
+  for(let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('mouseover', paintSquare);
   }
 }
 
 const squares = document.getElementsByClassName('square');
-
-for(let i = 0; i < squares.length; i++) {
-  squares[i].addEventListener('mouseover', paintSquare);
-}
-
 function paintSquare() {
   this.classList.add('hover');
 }
@@ -35,6 +32,16 @@ button.addEventListener('click', resetBoard);
 function resetBoard() {
   for(let i = 0; i < squares.length; i++) {
     squares[i].classList.remove('hover');
+  }
+
+  size = +prompt('How many number of squares per side do you want?')
+  deleteCurrentBoard();
+  generateBoard(size);
+}
+
+function deleteCurrentBoard() {
+  while (board.lastChild) {
+    board.removeChild(board.lastChild);
   }
 }
 
