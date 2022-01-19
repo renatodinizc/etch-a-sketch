8a28f01a3354a222996f3ba4a2b8ade6c8e0cd28
Renato Diniz <renatodiniz25@gmail.com>
2022-01-19 19:16:13 -0300

    Reset board button

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict';
 const board = document.getElementById('board');
+let size = 30;
 
-for(let i = 0; i < 16; i++) {
-  const div = document.createElement('div');
-  const column = board.appendChild(div);
-  column.classList.add('column')
-  column.classList.add(i)
-  for(let j = 0; j < 16; j++) {
+generateBoard(16);
+
+function generateBoard(size) {
+  for(let i = 0; i < size; i++) {
     const div = document.createElement('div');
-    const square = column.appendChild(div);
-    square.classList.add('square');
-    square.classList.add(`${j}x${i}`);
+    const column = board.appendChild(div);
+    column.classList.add('column')
+    column.classList.add(i)
+    for(let j = 0; j < size; j++) {
+      const div = document.createElement('div');
+      const square = column.appendChild(div);
+      square.classList.add('square');
+      square.classList.add(`${j}x${i}`);
+    }
   }
 }
 
@@ -23,5 +28,15 @@ for(let i = 0; i < squares.length; i++) {
 function paintSquare() {
   this.classList.add('hover');
 }
+
+const button = document.getElementById('button');
+button.addEventListener('click', resetBoard);
+
+function resetBoard() {
+  for(let i = 0; i < squares.length; i++) {
+    squares[i].classList.remove('hover');
+  }
+}
+
 
 
