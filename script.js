ad38068ea999b3fe3ad6fbdf80f14daca18794e9
Renato Diniz <renatodiniz25@gmail.com>
2022-01-27 17:37:55 -0300

    Add design and style

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 'use strict';
 const board = document.getElementById('board');
-let size = 16;
+let size = 24;
+let rainbowmode = false;
+
+const smallbtn = document.getElementById('small');
+const mediumbtn = document.getElementById('medium');
+const highbtn = document.getElementById('high');
+const classicbtn = document.getElementById('classic');
+const rainbowbtn = document.getElementById('rainbow');
+const erasebtn = document.getElementById('erase');
+
+erasebtn.addEventListener('click', resetBoard);
+smallbtn.addEventListener('click', resetBoard);
+mediumbtn.addEventListener('click', resetBoard);
+highbtn.addEventListener('click', resetBoard);
+classicbtn.addEventListener('click', colorMode);
+rainbowbtn.addEventListener('click', colorMode);
 
 generateBoard(size);
 
@@ -22,19 +37,40 @@ function generateBoard(size) {
 }
 
 const squares = document.getElementsByClassName('square');
+
 function paintSquare() {
-  this.classList.add('hover');
+  if ( rainbowmode == false) {
+    this.style.backgroundColor = 'black';
+  } else {
+    this.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
 }
 
-const button = document.getElementById('button');
-button.addEventListener('click', resetBoard);
+function colorMode() {
+  if (this.id == 'classic') {
+    rainbowmode = false;
+  } else {
+    rainbowmode = true;
+  }
+}
 
 function resetBoard() {
   for(let i = 0; i < squares.length; i++) {
-    squares[i].classList.remove('hover');
+    squares[i].classList.remove('blackpaint');
   }
-
-  size = +prompt('How many number of squares per side do you want?')
+  
+  switch (this.id) {
+    case 'small':
+      size = 16;
+      break;
+    case 'medium':
+      size = 24;
+      break;
+    case 'high':
+      size = 30;
+      break;
+  }
+  //size = +prompt('How many number of squares per side do you want?')
   deleteCurrentBoard();
   generateBoard(size);
 }
